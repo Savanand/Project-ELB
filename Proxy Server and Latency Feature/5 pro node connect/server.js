453c693f82cf71fdb86301d122b910d496dc7319
jaymehta1203 <jaymehta1203@gmail.com>
2014-11-12 20:57:24 -0800

    Proxy Server and Latency
    Proxy Server and Latency

---
+++ Proxy Server and Latency Feature/5 pro node connect/server.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var http = require('http')
+    httpProxy = require('http-proxy')
+var responseTime = require('response-time')
+var app = express()
+
+//
+// Create a proxy server with latency
+//
+var proxy = httpProxy.createProxyServer();
+
+//
+// Create your server that make an operation that take a while
+// and then proxy de request
+//
+app.use(responseTime())
+
+http.createServer(function (req, res) {
+  // This simulate an operation that take 5000ms in execute
+  setTimeout(function () {
+    proxy.web(req, res, {
+      target: 'http://localhost:9008'
+    });
+  }, 1000);
+
+	if (res._responseTime) return next();
+    res._responseTime = true;
+
+}).listen(8008);
+
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(9008);
